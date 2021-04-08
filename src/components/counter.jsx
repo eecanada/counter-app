b24d9d76dbb86a49299091041d913f9268e409e6
Eder Canada <canada.eder@gmail.com>
2021-04-07 21:31:08 -0700

    set badge classes based on counter ammount

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,24 +5,24 @@ class Counter extends Component {
     count: 0,
   }
 
-  styles = {
-    fontWeight: 10,
-    fontWeight: "bold"
-  }
-
   render() {
     return (
       <React.Fragment>
-        <span  style={{fontSize:30}} className="badge badge-primary m-2"> {this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm"> Increment </button>
       </React.Fragment>
     );
   }
 
   formatCount(){
-
     const {count} = this.state
     return count === 0 ? 'Zero' : count
+  }
+
+
+  getBadgeClasses(){
+    let classes = "badge m-2 badge-"
+    return classes += (this.state.count === 0 ? "warning" : "primary")
   }
 }
 
