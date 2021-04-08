e18a6107c3ee36fc05e42454671db531266fd036
Eder Canada <canada.eder@gmail.com>
2021-04-07 21:55:42 -0700

    render list of tags array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,27 +3,34 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-  }
+    tags: ['tag1', 'tag2', 'tag3'],
+  };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm"> Increment </button>
+        <ul>
+          {this.state.tags.map((tag) => {
+            return <li key={tag}> {tag} </li>;
+          })}
+        </ul>
       </React.Fragment>
     );
   }
 
-  formatCount(){
-    const {count} = this.state
-    return count === 0 ? 'Zero' : count
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 
-
-  getBadgeClasses(){
-    let classes = "badge m-2 badge-"
-    return classes += (this.state.count === 0 ? "warning" : "primary")
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    return (classes += this.state.count === 0 ? 'warning' : 'primary');
   }
 }
+
+  
 
 export default Counter;
