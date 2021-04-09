3d6d1179e695bdad5e7b7bae34b409a5f56f107b
Eder Canada <canada.eder@gmail.com>
2021-04-08 21:14:44 -0700

    add onClick to button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
+    tags: ['tag 1', 'tag 2', 'tag 3'],
   };
+
+
 
   renderTags(){
     if(this.state.tags.length === 0)  return <p> There are not tags!</p>
@@ -13,15 +15,34 @@ class Counter extends Component {
 
   }
 
+  handleIncrement(){
+    this.state.count = this.state.count + 1
+  }
+
+  
+
   render() {
     return (
       <div>
+        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
        {this.state.tags.length === 0 && "Please create a new tag"} 
        {this.renderTags()} 
+       <p> {this.state.count}</p>
       </div> 
     );
   }
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+  
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    return (classes += this.state.count === 0 ? 'warning' : 'primary');
+  }
  
 }
 
