508d2401d253670c3b446fc03716e89a875db25b
Eder Canada <canada.eder@gmail.com>
2021-04-08 21:59:59 -0700

    remove product

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -25,7 +25,10 @@ class Counter extends Component {
     return (
       <div>
         <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
-        <button onClick={ () => this.handleIncrement(product)} className="btn btn-secondary btn-sm"> Increment </button>
+        
+        {/* <button onClick={ () => this.handleIncrement(product)} className="btn btn-secondary btn-sm"> Increment </button> */}
+
+        <button onClick={ this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
        {this.state.tags.length === 0 && "Please create a new tag"} 
        {this.renderTags()} 
       <p> {this.state.count}</p>
