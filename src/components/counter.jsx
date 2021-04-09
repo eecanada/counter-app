c459e105587ea919b469d86af932585b6b4281f1
Eder Canada <canada.eder@gmail.com>
2021-04-08 21:28:01 -0700

    update count state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,7 +6,6 @@ class Counter extends Component {
     tags: ['tag 1', 'tag 2', 'tag 3'],
   };
 
-
   // constructor(){
   //   super()
   //     this.handleIncrement = this.handleIncrement.bind(this)
@@ -14,18 +13,12 @@ class Counter extends Component {
 
   renderTags(){
     if(this.state.tags.length === 0)  return <p> There are no tags!</p>
-
     return <ul> {this.state.tags.map((tag) => { return <li key={tag}> {tag} </li> })}</ul>
-
   }
-
-  
 
   handleIncrement = () => {
-    this.state.count = this.state.count + 1
+    this.setState({count: this.state.count + 1 })
   }
-
-  
 
   render() {
     return (
@@ -34,7 +27,7 @@ class Counter extends Component {
         <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm"> Increment </button>
        {this.state.tags.length === 0 && "Please create a new tag"} 
        {this.renderTags()} 
-      
+      <p> {this.state.count}</p>
       </div> 
     );
   }
@@ -44,12 +37,11 @@ class Counter extends Component {
     return count === 0 ? 'Zero' : count;
   }
   
-
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
     return (classes += this.state.count === 0 ? 'warning' : 'primary');
   }
- 
+
 }
 
   
