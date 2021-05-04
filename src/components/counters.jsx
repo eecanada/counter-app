59963b973a8dd43eb2b5095d3363aaa47364fa70
Eder Canada <canada.eder@gmail.com>
2021-05-03 21:07:49 -0700

    destructure props in NavBar and Counter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,25 +2,24 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
- 
-
   render() {
+    const {onReset, counters, onDelete, onIncrement} = this.props
     return (
       <div>
         <button
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.props.counters.map((counter) => {
+        {counters.map((counter) => {
           return (
             <Counter
               key={counter.id}
-              onDelete={this.props.onDelete}
+              onDelete={onDelete}
               counter={counter}
               eder={'eder'}
-              onIncrement={this.props.onIncrement}
+              onIncrement={onIncrement}
             />
           );
         })}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 
 //stateless functional component
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Navbar <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+        Navbar
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
         </span>
       </a>
     </nav>
   );
-}
-
+};
 
 export default NavBar;
-
