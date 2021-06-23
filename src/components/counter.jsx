a0249fa64c47e321bbb5739967c765265ede1f1c
Eder Canada <canada.eder@gmail.com>
2021-06-23 10:52:23 -0700

    style counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,29 +16,33 @@ class Counter extends Component {
 
   render() {
     return (
-      <div>
-        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
 
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          +
-        </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter.id)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.disableButton()}
+          >
+            -
+          </button>
 
-        <button
-          onClick={() => this.props.onDecrement(this.props.counter.id)}
-          className='btn btn-secondary btn-sm m-2 decrement' disabled={this.disableButton()}
-        >
-          -
-        </button>
-
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2" 
-        >
-          x
-        </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            x
+          </button>
+        </div>
       </div>
     );
   }
@@ -54,7 +58,7 @@ class Counter extends Component {
   }
 
   disableButton() {
-    return this.props.counter.value === 0
+    return this.props.counter.value === 0;
   }
 }
 
